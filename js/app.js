5b191fd7888aa866f4deba21ee4066d5f2d1e278
jackysee <jackysee@gmail.com>
2013-03-05 15:58:56 +0800

    move files and add tests

---
+++ js/app.js
@@ -0,0 +1,107 @@
+function App($scope, localStorageService){
+
+	$scope.settings = JSON.parse(localStorageService.get('settings')) || [];
+
+	$scope.setup = $scope.settings[0]? $scope.settings[0].setup : {
+		warmup: 180,
+		repeat: 8,
+		high: 10,
+		low: 10,
+		cooldown: 180
+	};
+
+	$scope.plan = {
+		count: 0, total:0,
+		progress: []
+	};
+
+	$scope.save = function(){
+		var name = prompt("Input a name for this setting:");
+		if(!name){
+			alert("Please input a name");
+			return;
+		}
+		$scope.settings.push({name:name, setup: angular.copy($scope.setup)});
+		localStorageService.add("settings", JSON.stringify($scope.settings));
+	};
+
+	$scope.load = function(index){
+		$scope.setup = angular.copy($scope.settings[index].setup);
+	};
+
+	$scope.remove = function(index){
+		var newSettings = [];
+		angular.forEach($scope.settings, function(s, i){
+			if(index !== i){
+				newSettings.push(angular.copy(s));
+			}
+		});
+		$scope.settings = newSettings;
+		localStorageService.add("settings", JSON.stringify($scope.settings));
+	};
+
+	$scope.clearAll = function(){
+		localStorageService.remove('settings');
+		$scope.settings = [];
+	};
+
+	$scope.$watch('setup', function(setup){
+		$scope.clear();
+		var progress = [], total = 0;
+		if(setup.warmup > 0){
+			progress.push({title:'Warm up', max:setup.warmup, value:0, start:1, end: setup.warmup, type:'warmup'});
+			total += setup.warmup;
+		}
+		for(var i=0; i<setup.repeat; i++){
+			progress.push({title:'High', max:setup.high, value:0, start:total+1, end:total+setup.high, type:'high'});
+			total += setup.high;
+			progress.push({title:'Low', max:setup.low, value:0, start:total+1, end:total+setup.low, type:'low'});
+			total += setup.low;
+		}
+		if(setup.cooldown > 0){
+			progress.push({title:'Cool down', max:setup.cooldown, value:0, start:total+1, end:total+setup.cooldown, type:'cooldown'});
+			total += setup.cooldown;
+		}
+		$scope.plan = {count:0, total:total, progress:progress};
+	}, true);
+
+	$scope.start = function(){
+		$scope.plan.count = 0;
+		$scope.plan.done = false;
+		$scope.plan.timer = setInterval(function(){
+			$scope.$apply(function(){
+				$scope.plan.count = $scope.plan.count + 1;
+				updateProgress();
+				if($scope.plan.count == $scope.plan.total){
+					clearInterval($scope.plan.timer);
+					$scope.plan.done = true;
+					delete $scope.plan.timer;
+				}
+			});
+		}, 1000);
+	};
+
+	$scope.clear = function(){
+		clearInterval($scope.plan.timer);
+		delete $scope.plan.timer;
+		$scope.plan.count = 0;
+		$scope.plan.current = null;
+		$scope.plan.done = false;
+		angular.forEach($scope.plan.progress, function(p){
+			p.value = 0;
+		});
+	};
+
+	function updateProgress(){
+		var progress = $scope.plan.progress,
+			count = $scope.plan.count;
+		for(var i=0; i<progress.length; i++){
+			var p = progress[i];
+			if(p.start <=  count && count <= p.end){
+				$scope.plan.current = p;
+				p.value = count - p.start + 1;
+				break;
+			}
+		}
+	}
+}
---
+++ test/spec/app_spec.js
@@ -0,0 +1,190 @@
+describe("App Controller", function(){
+
+	var scope, ls;
+
+	beforeEach(inject(function($controller, $rootScope) {
+		scope = $rootScope.$new();
+		ls = {
+			get: jasmine.createSpy('get').andReturn(null),
+			add: jasmine.createSpy('add'),
+			remove: jasmine.createSpy('remove')
+		};
+		$controller(App, {$scope:scope, localStorageService: ls})
+	}));
+
+	it("should be defined", function() {
+		expect(App).toBeDefined();
+	});
+
+	it("should init plan with zero", function($controller) {
+		expect(scope.plan.count).toEqual(0);
+		expect(scope.plan.total).toEqual(0);
+		expect(scope.plan.progress).toEqual([]);
+	});
+
+	it("should have initial setup", function() {
+		expect(scope.setup.warmup).toEqual(180);
+		expect(scope.setup.repeat).toEqual(8);
+		expect(scope.setup.high).toEqual(10);
+		expect(scope.setup.low).toEqual(10);
+		expect(scope.setup.cooldown).toEqual(180);
+	});
+
+	it("should create correct plan", function(){
+		scope.setup = {
+			warmup: 1,
+			repeat: 1,
+			high: 1,
+			low: 1,
+			cooldown: 1
+		};
+		scope.$digest();
+		expect(scope.plan.count).toEqual(0);
+		expect(scope.plan.total).toEqual(4);
+		var p = scope.plan.progress;
+		expect(p.length).toEqual(4);
+		expect(p[0].title).toEqual('Warm up');
+		expect(p[0].max).toEqual(1);
+		expect(p[0].value).toEqual(0);
+		expect(p[0].start).toEqual(1);
+		expect(p[0].end).toEqual(1);
+
+		expect(p[1].title).toEqual('High');
+		expect(p[1].max).toEqual(1);
+		expect(p[1].value).toEqual(0);
+		expect(p[1].start).toEqual(2);
+		expect(p[1].end).toEqual(2);
+
+		expect(p[2].title).toEqual('Low');
+		expect(p[2].max).toEqual(1);
+		expect(p[2].value).toEqual(0);
+		expect(p[2].start).toEqual(3);
+		expect(p[2].end).toEqual(3);
+
+		expect(p[3].title).toEqual('Cool down');
+		expect(p[3].max).toEqual(1);
+		expect(p[3].value).toEqual(0);
+		expect(p[3].start).toEqual(4);
+		expect(p[3].end).toEqual(4);
+	});
+
+	it("it should tick timer", function() {
+		jasmine.Clock.useMock();
+		scope.setup = {
+			warmup: 2, repeat: 2,
+			high: 2, low: 2, cooldown: 2
+		};
+		scope.$digest();
+		scope.start();
+		expect(scope.plan.count).toEqual(0);
+		expect(scope.plan.done).toEqual(false);
+		expect(scope.plan.timer).toBeDefined();
+
+		jasmine.Clock.tick(1000);
+		expect(scope.plan.count).toEqual(1);
+		expect(scope.plan.current).toEqual(scope.plan.progress[0]);
+		expect(scope.plan.progress[0].value).toEqual(1);
+
+		jasmine.Clock.tick(2000);
+		expect(scope.plan.count).toEqual(3);
+		expect(scope.plan.current).toEqual(scope.plan.progress[1]);
+		expect(scope.plan.progress[1].value).toEqual(1);
+
+		jasmine.Clock.tick(9000);
+		expect(scope.plan.count).toEqual(12);
+		expect(scope.plan.current).toEqual(scope.plan.progress[5]);
+		expect(scope.plan.progress[1].value).toEqual(2);
+		expect(scope.plan.done).toEqual(true);
+		expect(scope.plan.timer).not.toBeDefined();
+	});
+
+	it("should clear", function() {
+		jasmine.Clock.useMock();
+		scope.setup = {
+			warmup: 2, repeat: 2,
+			high: 2, low: 2, cooldown: 2
+		};
+		scope.$digest();
+		scope.start();
+		jasmine.Clock.tick(1000);
+		scope.clear();
+		expect(scope.plan.count).toEqual(0);
+		expect(scope.plan.current).toEqual(null);
+		expect(scope.plan.done).toEqual(false);
+		expect(scope.plan.timer).not.toBeDefined();
+		angular.forEach(scope.plan.progress, function(p){
+			expect(p.value).toEqual(0);
+		});
+	});
+
+	describe("local storage", function() {
+
+		var scope, ls;
+		beforeEach(inject(function($rootScope) {
+			scope = $rootScope.$new();
+			ls = {
+				get: jasmine.createSpy('get').andReturn(null),
+				add: jasmine.createSpy('add'),
+				remove: jasmine.createSpy('remove')
+			};
+			spyOn(window, 'alert');
+			spyOn(window, 'prompt');
+		}));
+
+		it("should load default settings", inject(function($controller) {
+			var s = {
+				name:'hi',
+				setup:{warmup: 2, repeat:2, high: 2, low:2, cooldown:2}
+			};
+			ls.get.andReturn(JSON.stringify([s]));
+			$controller(App, {$scope:scope, localStorageService: ls})
+			expect(scope.settings.length).toEqual(1);
+			expect(scope.settings[0].name).toEqual('hi');
+			expect(scope.settings[0].setup).toEqual(s.setup);
+		}));
+
+		it("should save current setting", inject(function($controller) {
+			$controller(App, {$scope:scope, localStorageService: ls});
+			scope.setup = {warmup:3, repeat:3, high:3, low:3, cooldown:3};
+			window.prompt.andReturn("test name");
+			expect(scope.settings.length).toEqual(0);
+			scope.save();
+			expect(scope.settings.length).toEqual(1);
+			expect(scope.settings[0].name).toEqual("test name");
+			expect(scope.settings[0].setup).toEqual(scope.setup);
+			expect(ls.add).toHaveBeenCalledWith('settings', JSON.stringify(scope.settings));
+		}));
+
+		it("should not save if no name is provided", inject(function($controller) {
+			$controller(App, {$scope:scope, localStorageService: ls});
+			window.prompt.andReturn("");
+			scope.save();
+			expect(window.alert).toHaveBeenCalledWith("Please input a name");
+		}));
+
+		it("should load by index", inject(function($controller) {
+			ls.get.andReturn(JSON.stringify([
+				{name:'1', setup:{warmup:1,repeat:1,high:1,low:1,cooldown:1}},
+				{name:'2', setup:{warmup:2,repeat:2,high:2,low:2,cooldown:2}}
+			]));
+			$controller(App, {$scope:scope, localStorageService: ls});
+			scope.load(1);
+			expect(scope.setup.warmup).toEqual(2);
+		}));
+
+		it("should remove by index", inject(function($controller) {
+			ls.get.andReturn(JSON.stringify([
+				{name:'1', setup:{warmup:1,repeat:1,high:1,low:1,cooldown:1}},
+				{name:'2', setup:{warmup:2,repeat:2,high:2,low:2,cooldown:2}}
+			]));
+			$controller(App, {$scope:scope, localStorageService: ls});
+			scope.remove(1);
+			expect(scope.settings.length).toEqual(1);
+			expect(ls.add).toHaveBeenCalledWith('settings', JSON.stringify(scope.settings));
+		}));
+	});
+
+
+
+
+});
